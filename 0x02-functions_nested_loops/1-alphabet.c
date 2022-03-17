df77c4cc7b4d75da9ce756de3fa8aa3930861156
oyindoubra2 <lois.ziboh@physci.uniben.edu>
2022-03-16 23:43:50 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_alphabeth - print alphabet lowercase
+  *
+  * Return: Always zero
+  */
+
+void print_alphabet(void)
+{
+	char alphabet;
+	for (alphabet = 'a' ; alphabte <= 'z' ; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+}
