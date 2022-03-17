2a414df18ba948cd552c3a02dc3ab66a91b33273
oyindoubra2 <lois.ziboh@physci.uniben.edu>
2022-03-16 22:40:34 -0700

    main header

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -7,7 +6,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack-bauer(void);
+void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
