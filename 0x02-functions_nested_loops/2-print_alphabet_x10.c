6307caef23d28cf27a84b1e506cb61e9947629f7
oyindoubra2 <lois.ziboh@physci.uniben.edu>
2022-03-17 00:48:51 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,6 @@ void print_alphabet_x10(void)
 	int alphabet;
 	int count;
 
-
 	count = 0;
 	while (count < 10)
 	{ 
@@ -20,6 +19,6 @@ void print_alphabet_x10(void)
 			_putchar(alphabet);
 		}
 		count++;
-			_putchar('\n');
+		_putchar('\n');
 	}
 }
