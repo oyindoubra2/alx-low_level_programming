adbc87d8929c2b346a19226f4d17c42e12f15713
oyindoubra2 <lois.ziboh@physci.uniben.edu>
2022-03-22 14:39:07 -0700

    Pointer task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,12 +3,12 @@
 /**
   * _strlen - returns the length of a string
   * @s: string is evaluated
-  * Returns: the length of string
+  * Return: the length of string
   */
 
 int _strlen(char *s)
 {
-I	int i;
+	int i;
 
 	while ( i = 0; str[i] != '\0')
 	{
