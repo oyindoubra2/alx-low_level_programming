28143bf7ecdff0e92360f1e6a0234fd81cf5c8cb
oyindoubra2 <lois.ziboh@physci.uniben.edu>
2022-03-22 14:29:00 -0700

    Pointer task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * strlen - returns the length of a string
+  * @s: string is evaluated
+  * Returns: the length of string
+  */
+
+int _strlen(char *s);
+{
+	int i;
+
+	while ( i = o; str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
