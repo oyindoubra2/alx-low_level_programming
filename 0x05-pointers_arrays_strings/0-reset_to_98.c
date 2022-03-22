80de5f17e2c7e7cf9bbffe8ea4673d4f0df784a7
oyindoubra2 <lois.ziboh@physci.uniben.edu>
2022-03-22 13:08:46 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* reset_to_98 - update an integer value to 98
+* @n: a pointer to an int that will be updated
+* Return: always 0
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
